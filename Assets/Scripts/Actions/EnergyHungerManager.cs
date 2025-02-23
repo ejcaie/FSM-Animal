1f28f4219eb7e2537e9f0d048accfbfa21d1dfd0
ejcaie <evancaie705@gmail.com>
2025-02-23 18:32:29 -0500

    Continued organizing states

--- Assets/Scripts/Actions/EnergyHungerManager.cs
+++ Assets/Scripts/Actions/EnergyHungerManager.cs
@@ -1,12 +1,14 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
 using UnityEngine;
-using UnityEngine.AI;
 
 
 namespace NodeCanvas.Tasks.Actions {
 
-	public class Search : ActionTask {
+	public class EnergyHungerManager : ActionTask {
+
+		public BBParameter<float> hunger;
+		public BBParameter<float> energy;
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -18,12 +20,21 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			//EndAction(true);
+
 		}
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
+			
+			if (hunger.value >= 0)
+			{
+				hunger.value -= hunger.value;
+			}
 
+			if (energy.value >= 0)
+			{
+				energy.value -= energy.value;
+            }
 		}
 
 		//Called when the task is disabled.
--- Assets/Scripts/Conditions/IsAlerted.cs
+++ Assets/Scripts/Conditions/IsAlerted.cs
@@ -25,7 +25,7 @@ namespace NodeCanvas.Tasks.Conditions {
 		//Called once per frame while the condition is active.
 		//Return whether the condition is success or failure.
 		protected override bool OnCheck() {
-			return true;
+			return false;
 		}
 	}
 }
