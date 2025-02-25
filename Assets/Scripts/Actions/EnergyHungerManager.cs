d4e267dbae49b448eb5d6597cdd9d84e5481af99
ejcaie <evancaie705@gmail.com>
2025-02-25 18:22:18 -0500

    AM I SMART OR AM I SMART (i'm not)

--- Assets/Scripts/Actions/EnergyHungerManager.cs
+++ Assets/Scripts/Actions/EnergyHungerManager.cs
@@ -1,5 +1,6 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -9,21 +10,8 @@ namespace NodeCanvas.Tasks.Actions {
 
 		public BBParameter<float> hunger;
 		public BBParameter<float> energy;
+		public Collider trigger;
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit() {
-			return null;
-		}
-
-		//This is called once each time the task is enabled.
-		//Call EndAction() to mark the action as finished, either in success or failure.
-		//EndAction can be called from anywhere.
-		protected override void OnExecute() {
-
-		}
-
-		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
 			
 			if (hunger.value >= 0)
@@ -39,16 +27,6 @@ namespace NodeCanvas.Tasks.Actions {
 
                 if (energy.value < 0.1) energy.value = 0;
             }
-		}
-
-		//Called when the task is disabled.
-		protected override void OnStop() {
-			
-		}
-
-		//Called when the task is paused.
-		protected override void OnPause() {
-			
 		}
 	}
 }
